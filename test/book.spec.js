3607debd0a55d668146a301a305d5486380ca331
orewanf <orewanf123@gmail.com>
2017-09-23 18:42:35 +0700

    add new test

--- test/book.spec.js
+++ test/book.spec.js
@@ -1,12 +1,31 @@
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+
+var mongoose = require('mongoose');
+require('sinon-mongoose');
 var Book = require('../models/book');
 
-describe('book', function () {
+describe('Book', function () {
     it('Should be invalid if all properties is empty', function (done) {
         var newBook = new Book();
 
         newBook.validate(function (err) {
             expect(err.errors).to.exist;
+            done();
+        });
+    });
+
+    it('Should be invalid if year is string', function (done) {
+        var newBook = new Book({
+            title: 'test',
+            author: 'test',
+            genre: 'test',
+            year: '1'
+        });
+
+        newBook.validate(function (err) {
+            expect(err.errors.year).to.exist;
             done();
         });
     });
